b7fb62d08613dc009d1cbf2800cc3aadbf86d4b9
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-21 06:07:40 +0800

    setup the home with header component

---
+++ components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import Header from '../Header/Header'
+
+const Home = () => {
+  return (
+    <View>
+        <Header/>
+        <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
