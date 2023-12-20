6347e7c5a4ee4493004a12187f7250f8c8d899c2
Earl Hans Genoso <earlhansg@gmail.com>
2023-12-20 22:40:02 +0800

    added TouchableHighlight in User Component

--- components/User/User.tsx
+++ components/User/User.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View } from "react-native";
 import InfoList from "./InfoList/InfoList";
 import { UserStyle } from "./UserStyle";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { ThemeContext } from "../ThemeContext/ThemeContext";
 
 const User = () => {
+  const theme = useContext(ThemeContext);
+
+  const navigate = () => {
+    console.log("im click in Try");
+  };
   return (
-    <View style={UserStyle.container}>
-        <InfoList />
-    </View>
+    <TouchableHighlight style={UserStyle.container} underlayColor={theme.gray.light} onPress={navigate}>
+      <InfoList />
+    </TouchableHighlight>
   )
 }
 
--- pages/Profile/Profile.tsx
+++ pages/Profile/Profile.tsx
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, TouchableHighlight } from "react-native";
 import { ProfileStyle } from "./ProfileStyle";
 import ButtonIcon from "../../components/Buttons/ButtonIcon";
 import { Feather } from "@expo/vector-icons";
 import { Navigation } from "../model/Navigation";
 import { ThemeContext } from "../../components/ThemeContext/ThemeContext";
 import User from "../../components/User/User";
-
 
 const Profile = ({ navigation }: Navigation) => {
   const theme = useContext(ThemeContext);
@@ -16,20 +15,22 @@ const Profile = ({ navigation }: Navigation) => {
   };
   return (
     <SafeAreaView style={ProfileStyle.container}>
-      <View
-        style={ProfileStyle.headerContainer}
-      >
+      <View style={ProfileStyle.headerContainer}>
         <View style={ProfileStyle.headerIconContainer}>
-            <ButtonIcon
+          <ButtonIcon
             renderIcon={(settings) => (
-                <Feather name="x" color={theme.primary.color} size={settings.isClicked ? 17 : 20} />
+              <Feather
+                name="x"
+                color={theme.primary.color}
+                size={settings.isClicked ? 17 : 20}
+              />
             )}
             onClick={handleClickMenuBar}
-            />
+          />
         </View>
         <Text style={ProfileStyle.headerText}>Profile</Text>
       </View>
-      <User/>
+      <User />
     </SafeAreaView>
   );
 };
