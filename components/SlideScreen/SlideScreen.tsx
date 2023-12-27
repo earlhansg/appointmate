d71d8dc6eec3875d38437b9dc153810afcd59c81
Earl Hans Genoso <earlhansg@gmail.com>
2023-12-27 23:50:55 +0800

    added SlideScreen Component

---
+++ components/SlideScreen/SlideScreen.tsx
@@ -0,0 +1,94 @@
+import React, { useContext } from 'react'
+import { Pressable, Text, View } from "react-native";
+import { ThemeContext } from '../ThemeContext/ThemeContext';
+import { Navigation } from '../../pages/model/Navigation';
+import { ShopDeal } from '../Deals/model/ShopDeal';
+import ShopDeals from '../Deals/ShopDeals/ShopDeals';
+
+type SlideScreenProps = {
+    navigation?: Navigation;
+    shopDeals: ShopDeal[];
+}
+
+const SlideScreen = ({ navigation, shopDeals }: SlideScreenProps) => {
+const theme = useContext(ThemeContext);
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          // backgroundColor: "red",
+          alignItems: "center",
+          paddingLeft: 15,
+          paddingRight: 15,
+          paddingTop: 15,
+          paddingBottom: 10,
+          gap: 10
+        }}
+      >
+        <Pressable
+          style={[
+            {
+              backgroundColor: theme.primary.color,
+            },
+            {
+              alignItems: "center",
+              paddingTop: 10,
+              paddingBottom: 10,
+              paddingLeft: 10,
+              paddingRight: 10,
+              borderRadius: 25
+            },
+          ]}
+        >
+          <Text
+            style={[
+              { color: theme.white.color },
+              {
+                fontWeight: "500",
+                fontSize: 13,
+              },
+            ]}
+          >
+            Home Service
+          </Text>
+        </Pressable>
+        <Pressable
+          style={[
+            {
+              backgroundColor: theme.white.color,
+              borderColor: theme.gray.light2,
+              borderWidth: 1
+            },
+            {
+              alignItems: "center",
+              paddingTop: 10,
+              paddingBottom: 10,
+              paddingLeft: 10,
+              paddingRight: 10,
+              borderRadius: 25
+            },
+          ]}
+        >
+          <Text
+            style={[
+              { color: theme.black.dark },
+              {
+                fontWeight: "500",
+                fontSize: 13,
+              },
+            ]}
+          >
+            Walk In
+          </Text>
+        </Pressable>
+      </View>
+      <View>
+          <ShopDeals shopDeals={shopDeals} showInHorizontal={false}/>
+      </View>
+    </View>
+  )
+}
+
+export default SlideScreen
--- pages/Favourites/Favourites.tsx
+++ pages/Favourites/Favourites.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Pressable, SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import { FavouritesStyle } from "./FavoritesStyle";
 import { ThemeContext } from "../../components/ThemeContext/ThemeContext";
 import ButtonIcon from "../../components/Buttons/ButtonIcon";
@@ -7,96 +7,9 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Navigation } from "../model/Navigation";
 
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { ShopDeal } from "../../components/Deals/model/ShopDeal";
-import ShopDeals from "../../components/Deals/ShopDeals/ShopDeals";
+import SlideScreen from "../../components/SlideScreen/SlideScreen";
 
 const Tab = createMaterialTopTabNavigator();
-
-type SlideScreenProps = {
-  navigation?: Navigation;
-  shopDeals: ShopDeal[];
-}
-
-function SlideScreen({ navigation, shopDeals }: SlideScreenProps) {
-  const theme = useContext(ThemeContext);
-  return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          width: "100%",
-          // backgroundColor: "red",
-          alignItems: "center",
-          paddingLeft: 15,
-          paddingRight: 15,
-          paddingTop: 15,
-          paddingBottom: 10,
-          gap: 10
-        }}
-      >
-        <Pressable
-          style={[
-            {
-              backgroundColor: theme.primary.color,
-            },
-            {
-              alignItems: "center",
-              paddingTop: 10,
-              paddingBottom: 10,
-              paddingLeft: 10,
-              paddingRight: 10,
-              borderRadius: 25
-            },
-          ]}
-        >
-          <Text
-            style={[
-              { color: theme.white.color },
-              {
-                fontWeight: "500",
-                fontSize: 13,
-              },
-            ]}
-          >
-            Home Service
-          </Text>
-        </Pressable>
-        <Pressable
-          style={[
-            {
-              backgroundColor: theme.white.color,
-              borderColor: theme.gray.light2,
-              borderWidth: 1
-            },
-            {
-              alignItems: "center",
-              paddingTop: 10,
-              paddingBottom: 10,
-              paddingLeft: 10,
-              paddingRight: 10,
-              borderRadius: 25
-            },
-          ]}
-        >
-          <Text
-            style={[
-              { color: theme.black.dark },
-              {
-                fontWeight: "500",
-                fontSize: 13,
-              },
-            ]}
-          >
-            Walk In
-          </Text>
-        </Pressable>
-      </View>
-      <View>
-          <ShopDeals shopDeals={shopDeals} showInHorizontal={false}/>
-      </View>
-    </View>
-  );
-}
 
 const Favourites = ({ navigation }: Navigation) => {
   const theme = useContext(ThemeContext);
