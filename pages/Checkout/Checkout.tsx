f58d76e29ff3d7d2a8aa9d77bae8b8165d2c2ec1
Earl Hans Genoso <earlhansg@gmail.com>
2024-01-02 02:37:03 +0800

    added UI for Review and Deals in Checkout Page

--- pages/Checkout/Checkout.tsx
+++ pages/Checkout/Checkout.tsx
@@ -2,11 +2,13 @@
 import { Image, SafeAreaView, Text, View, StyleSheet } from "react-native";
 import { CheckoutStyle } from "./CheckoutStyle";
 import ButtonIcon from "../../components/Buttons/ButtonIcon";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { CheckoutData, Navigation } from "../model/Navigation";
 import { ThemeContext } from "../../components/ThemeContext/ThemeContext";
 import { useRoute, RouteProp } from "@react-navigation/native";
 import { Shop } from "../../components/TopShops/model/Shop";
+
+import { Feather, FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 type CategoryRouteProps = {
   route: RouteProp<
@@ -55,9 +57,6 @@ const Checkout = ({navigation}: Navigation) => {
             height: 35,
           }}
         />
-        {/* <Text style={{fontSize: 16, fontWeight: "500", textAlignVertical: "center"}}>
-          Burger King - Divisoria
-        </Text> */}
         {('name' in checkoutData) && <Text style={{fontSize: 16, fontWeight: "500", textAlignVertical: "center"}}>{(checkoutData as Shop).name}</Text>}
       </View>
       <View style={{flexDirection: "row", gap: 5, paddingLeft: 15, paddingRight: 15, paddingTop: 10}}>
@@ -70,9 +69,37 @@ const Checkout = ({navigation}: Navigation) => {
         <Text style={{fontSize: 11, fontWeight: "500", color: theme.gray.light3, paddingRight: 3, paddingLeft: 3, marginRight: "auto"}}>
           &#8369; 119.00 Minimum
         </Text>
-        <Text style={{fontSize: 11, fontWeight: "500", color: theme.primary.color}}>
+        <Text style={{fontSize: 11, fontWeight: "500", color: theme.primary.color, textAlignVertical: "center"}}>
           More Info
         </Text>
+      </View>
+      <View>
+        <View style={{flexDirection: "row", paddingLeft: 15, paddingRight: 15, paddingTop: 15}}>
+          <Feather name="star" size={18} color={theme.primary.color} />
+          <Text style={{fontSize: 14, color: theme.black.dark, fontWeight: "500", marginLeft: 8, textAlignVertical: "center"}}>4.5</Text>
+          <Text style={{fontSize: 11, color: theme.gray.light3, fontWeight: "500", marginLeft: 10, textAlignVertical: "center", marginRight: "auto"}}>5000+ ratings</Text>
+          <Text style={{fontSize: 11, fontWeight: "500", color: theme.primary.color, textAlignVertical: "center"}}>See reviews</Text>
+        </View>
+        <View style={{flexDirection: "row", paddingLeft: 15, paddingRight: 15, paddingTop: 15}}>
+          <FontAwesome5 name="tags" size={14} color={theme.primary.color}/>
+          <Text style={{fontSize: 14, color: theme.black.dark, fontWeight: "500", marginLeft: 7, textAlignVertical: "center"}}>Available deals</Text>
+        </View>
+        <View style={{flexDirection: "row", paddingLeft: 15, paddingRight: 15, paddingTop: 15}}>
+          <View style={{width: "100%", flexDirection: "row", backgroundColor: "red"}}>
+            <View style={{flexDirection: "row", backgroundColor: "skyblue", padding: 10, width: 176, marginRight: 11}}>
+              <MaterialCommunityIcons name="octagram" size={15} color={theme.primary.color} />
+              <Text style={{fontSize: 11, color: theme.black.dark, fontWeight: "500"}}>20% Off</Text>
+            </View>
+            <View style={{flexDirection: "row", backgroundColor: "skyblue", padding: 10, width: 176}}>
+              <MaterialCommunityIcons name="octagram" size={15} color={theme.primary.color} />
+              <Text style={{fontSize: 11, color: theme.black.dark, fontWeight: "500"}}>20% Off</Text>
+            </View>
+            {/* <View style={{flexDirection: "row", backgroundColor: "skyblue", padding: 10, width: "48%"}}>
+              <MaterialCommunityIcons name="octagram" size={15} color={theme.primary.color} />
+              <Text style={{fontSize: 11, color: theme.black.dark, fontWeight: "500"}}>20% Off</Text>
+            </View> */}
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
