c859b1fde70fd21a102973c623d7e0b1070fbee8
Earl Hans Genoso <earlhansg@gmail.com>
2024-01-14 23:02:12 +0800

    initial design for ServiceChekout header

--- pages/ServiceCheckout/ServiceCheckout.tsx
+++ pages/ServiceCheckout/ServiceCheckout.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext } from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+import ButtonIcon from '../../components/Buttons/ButtonIcon'
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { ServiceCheckoutStyle } from './ServiceCheckoutStyle';
+import { ThemeContext } from '../../components/ThemeContext/ThemeContext';
 
 const ServiceCheckout = () => {
+  const theme = useContext(ThemeContext);
   return (
-    <Text>ServiceCheckout</Text>
+    <SafeAreaView style={ServiceCheckoutStyle.container}>
+        <View style={ServiceCheckoutStyle.headerContainer}>
+          <View style={ServiceCheckoutStyle.headerIconContainer}>
+            <ButtonIcon
+              renderIcon={(settings) => (
+                <MaterialCommunityIcons
+                  name="arrow-left"
+                  color={theme.primary.color}
+                  size={20}
+                />
+              )}
+              // onClick={handleClickMenuBar}
+            />
+          </View>
+          {/* <Text style={ServiceCheckoutStyle.headerText}>{name}</Text> */}
+        </View>
+      </SafeAreaView>
   )
 }
 
