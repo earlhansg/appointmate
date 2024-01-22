b0655d4fa96a40886f4c69cfff7541b68b515e48
Earl Hans Genoso <earlhansg@gmail.com>
2024-01-22 23:19:27 +0800

    added Calendar in ServiceCheckout

--- pages/ServiceCheckout/ServiceCheckout.tsx
+++ pages/ServiceCheckout/ServiceCheckout.tsx
@@ -8,6 +8,7 @@ import { ThemeContext } from "../../components/ThemeContext/ThemeContext";
 import { Navigation, ServiceCheckoutData } from "../model/Navigation";
 
 import { useRoute, RouteProp } from "@react-navigation/native";
+import { Calendar } from "react-native-calendars";
 
 type ServiceRouteProps = {
   route: RouteProp<
@@ -26,15 +27,21 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
 
   const aircon = require("../../assets/checkout-images/air-conditioner-3.png");
 
+
+  const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
+const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
+const workout = {key: 'workout', color: 'green'};
   return (
     <SafeAreaView style={ServiceCheckoutStyle.container}>
-      <View style={ServiceCheckoutStyle.headerContainer}>
-        <ScrollView stickyHeaderIndices={[1]} scrollEventThrottle={16} showsVerticalScrollIndicator={false}>
-          <View
-            style={[
-              ServiceCheckoutStyle.headerIconContainer
-            ]}
-          >
+      <View
+        style={ServiceCheckoutStyle.headerContainer}
+      >
+        <ScrollView
+          stickyHeaderIndices={[1]}
+          scrollEventThrottle={16}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={[ServiceCheckoutStyle.headerIconContainer]}>
             <Image
               source={aircon}
               resizeMode="contain"
@@ -61,29 +68,32 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
               />
             </View>
           </View>
-          {/* <Text style={ServiceCheckoutStyle.headerText}>Test</Text> */}
           <View
             style={{
               padding: 10,
               borderBottomWidth: StyleSheet.hairlineWidth,
               borderColor: "#e0e0e0",
-              backgroundColor: "white"
+              backgroundColor: "white",
             }}
           >
-            <View style={{flexDirection: "row",}}>
+            <View style={{ flexDirection: "row" }}>
               <View
                 style={{
-                  width: 270
+                  width: 270,
                 }}
               >
                 <Text style={{ fontSize: 20, fontWeight: "500" }}>
                   Cleaning 1
                 </Text>
                 <Text
-                  style={{ marginTop: 5, fontSize: 13, color: theme.gray.light3 }}
+                  style={{
+                    marginTop: 5,
+                    fontSize: 13,
+                    color: theme.gray.light3,
+                  }}
                 >
-                  Cleaning 1 Cleaning 1 Cleaning 1 Cleaning 1 Cleaning 1 Cleaning
-                  1 Cleaning 1
+                  Cleaning 1 Cleaning 1 Cleaning 1 Cleaning 1 Cleaning 1
+                  Cleaning 1 Cleaning 1
                 </Text>
               </View>
               <Text
@@ -92,7 +102,7 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
                   textAlign: "center",
                   marginTop: 10,
                   fontSize: 13,
-                  fontWeight: "500"
+                  fontWeight: "500",
                 }}
               >
                 156.00
@@ -118,7 +128,7 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
                 fontWeight: "500",
               }}
             >
-              Service Options
+              Service options
             </Text>
             <View
               style={{
@@ -293,8 +303,7 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
             </View>
           </View>
 
-          <View
-            style={{
+          <View style={{
               margin: 10,
               paddingBottom: 10,
               borderTopWidth: 1,
@@ -303,169 +312,40 @@ const ServiceCheckout = ({ navigation }: Navigation) => {
               borderRightWidth: 2,
               borderColor: theme.gray.light1,
               borderRadius: 5,
-            }}
-          >
-            <View style={{ flexDirection: "row", padding: 10 }}>
+            }}>
+
+            <View style={{ flexDirection: "row", paddingTop: 10, paddingLeft: 10, paddingRight: 10 }}>
               <View
                 style={{ flexDirection: "row", gap: 5, marginRight: "auto" }}
               >
-                <Feather name="map-pin" size={20} color={theme.primary.color} />
+                <MaterialIcons name="schedule" size={20} color={theme.primary.color} />
                 <Text
                   style={{
                     fontSize: 14,
                     fontWeight: "500",
                   }}
                 >
-                  Delivery address
+                  Select schedule
                 </Text>
               </View>
-              <Octicons
-                name="pencil"
-                size={20}
-                color={theme.primary.color}
-                style={{ alignSelf: "flex-start" }}
-              />
             </View>
-            <View
-              style={{
-                backgroundColor: theme.gray.light1,
-                marginRight: 10,
-                marginLeft: 10,
+            
+            <Calendar
+              minDate={"2024-01-01"}
+              onDayPress={(day) => {
+                console.log("selected day", day);
               }}
-            >
-              <View
-                style={{
-                  width: "100%",
-                  height: 70,
-                  borderRadius: 5,
-                }}
-              ></View>
-            </View>
-
-            <View
-              style={{
-                borderBottomWidth: 1,
-                borderBottomColor: theme.gray.light1,
-                padding: 10,
+              enableSwipeMonths={true}
+              markedDates={{
+                "2024-01-25": {
+                  dots: [vacation, massage, workout],
+                  disabled: true,
+                },
+                "2024-01-26": { dots: [massage, workout], disabled: true },
               }}
-            >
-              <Text style={{ fontSize: 12, fontWeight: "500" }}>Home</Text>
-              <Text style={{ fontSize: 12 }}>Five Distribution Rectan</Text>
-              <Text style={{ fontSize: 12 }}>Cagayan de Oro City</Text>
-            </View>
-            <View
-              style={{
-                paddingTop: 10,
-                paddingBottom: 10,
-                paddingLeft: 15,
-                paddingRight: 15,
-                flexDirection: "row",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "500",
-                  color: theme.gray.light3,
-                  marginRight: "auto",
-                }}
-              >
-                Frankee's Bar
-              </Text>
-              <MaterialIcons
-                name="keyboard-arrow-right"
-                size={20}
-                color={theme.primary.color}
-              />
-            </View>
+            />
           </View>
 
-          <View
-            style={{
-              margin: 10,
-              paddingBottom: 10,
-              borderTopWidth: 1,
-              borderBottomWidth: 2,
-              borderLeftWidth: 2,
-              borderRightWidth: 2,
-              borderColor: theme.gray.light1,
-              borderRadius: 5,
-            }}
-          >
-            <View style={{ flexDirection: "row", padding: 10 }}>
-              <View
-                style={{ flexDirection: "row", gap: 5, marginRight: "auto" }}
-              >
-                <Feather name="map-pin" size={20} color={theme.primary.color} />
-                <Text
-                  style={{
-                    fontSize: 14,
-                    fontWeight: "500",
-                  }}
-                >
-                  Delivery address
-                </Text>
-              </View>
-              <Octicons
-                name="pencil"
-                size={20}
-                color={theme.primary.color}
-                style={{ alignSelf: "flex-start" }}
-              />
-            </View>
-            <View
-              style={{
-                backgroundColor: theme.gray.light1,
-                marginRight: 10,
-                marginLeft: 10,
-              }}
-            >
-              <View
-                style={{
-                  width: "100%",
-                  height: 70,
-                  borderRadius: 5,
-                }}
-              ></View>
-            </View>
-
-            <View
-              style={{
-                borderBottomWidth: 1,
-                borderBottomColor: theme.gray.light1,
-                padding: 10,
-              }}
-            >
-              <Text style={{ fontSize: 12, fontWeight: "500" }}>Home</Text>
-              <Text style={{ fontSize: 12 }}>Five Distribution Rectan</Text>
-              <Text style={{ fontSize: 12 }}>Cagayan de Oro City</Text>
-            </View>
-            <View
-              style={{
-                paddingTop: 10,
-                paddingBottom: 10,
-                paddingLeft: 15,
-                paddingRight: 15,
-                flexDirection: "row",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "500",
-                  color: theme.gray.light3,
-                  marginRight: "auto",
-                }}
-              >
-                Frankee's Bar
-              </Text>
-              <MaterialIcons
-                name="keyboard-arrow-right"
-                size={20}
-                color={theme.primary.color}
-              />
-            </View>
-          </View>
         </ScrollView>
       </View>
     </SafeAreaView>
